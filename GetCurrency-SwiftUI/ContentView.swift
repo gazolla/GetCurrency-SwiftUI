43af6422b31f47bc8dca32f394f5bf1e3332f9a7
Sebastiao Gazolla Costa Junior <gazolla@gmail.com>
2022-06-02 19:23:05 -0300

    Initial Commit

---
+++ GetCurrency-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GetCurrency-SwiftUI
+//
+//  Created by sebastiao Gazolla Costa Junior on 02/06/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GetCurrency-SwiftUI/GetCurrency_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  GetCurrency_SwiftUIApp.swift
+//  GetCurrency-SwiftUI
+//
+//  Created by sebastiao Gazolla Costa Junior on 02/06/22.
+//
+
+import SwiftUI
+
+@main
+struct GetCurrency_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
